d3e65abba3c6b765306a6add93c536cab4ba3aa7
AlexandreBarat <alexandre.barat@hardis.fr>
2018-06-21 18:02:36 +0200

    Add provider.aws: version

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   access_key = "AKIAJL5JVZQJ5YS4364A"                     # User admin_terraform
   secret_key = "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"
   region     = "eu-west-1"                                # Region Ireland
+  version    = "~> 1.24"
 }
 
 resource "aws_vpc" "web_vpc" {
