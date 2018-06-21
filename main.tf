ef4a7ad3266e4a7ebcea15833bcfa82d412bef56
AlexandreBarat <40398545+AlexandreBarat@users.noreply.github.com>
2018-06-21 15:27:06 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "AKIAJL5JVZQJ5YS4364A"                     # User admin_terraform
+  secret_key = "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"
+  region     = "eu-west-1"                                # Region Ireland
+}
+
+resource "aws_vpc" "web_vpc" {
+  cidr_block           = "192.168.100.0/24"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "Web VPC"
+  }
+}
